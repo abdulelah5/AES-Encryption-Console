8dc782323b05ef805204d2831bcdfe126225f0f9
Abdulelah <AbdulelahAng@gmail.com>
2024-07-04 14:07:29 +0300

    Add the project

---
+++ EncryptionTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Claims;
+using System.Web;
+
+namespace EncryptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region AES Encryption
+
+            //your Key
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+
+            //string to encrypt
+            Console.WriteLine("Please enter a string to encrypt:");
+            string plaintext = Console.ReadLine();
+            Console.WriteLine("");
+
+            //encryption value
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = StringCipher.Encrypt(plaintext, password);
+            Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            //decryption
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+            Console.WriteLine(decryptedstring);
+
+            #endregion
+
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }   
+}
